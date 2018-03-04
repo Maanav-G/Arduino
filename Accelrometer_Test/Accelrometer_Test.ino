10c080f3d56b1ff30873b03041825a9863fd8787
Maanav Garg <35978979+Maanav-G@users.noreply.github.com>
2018-03-03 20:34:15 -0500

    Update Accelrometer_Test.ino

--- Accelrometer_Test/Accelrometer_Test.ino
+++ Accelrometer_Test/Accelrometer_Test.ino
@@ -1,30 +1,38 @@
+/*
+The Curie IMU library enables an Arduino or Genuino 101 to read the output of the on-board 
+IMU (Inertial Measurement Unit) containing an accelerometer and a gyroscope and elaborate 
+the raw data coming from it. - only compatible with Arduino or Genuino 101
+ */
+
 #include "CurieIMU.h"
 
 void setup() {
-  Serial.begin(9600); // initialize Serial communication
-  while (!Serial);    // wait for the serial port to open
+  Serial.begin(9600); // initialize baud value
+  while (!Serial);    // activate only when serial port is open
 
-  // initialize device
-  Serial.println("Initializing IMU device...");
+  // start the accelerometer device
+  Serial.println("Starting the device...");
   CurieIMU.begin();
 
-  // Set the accelerometer range to 2G
+  // set.Accelerometer sets the range of the accelerometer as multiples of g, in this case range is 2G
   CurieIMU.setAccelerometerRange(2);
 }
 
 void loop() {
-  float ax, ay, az;   //scaled accelerometer values
+  float ax, ay, az;   //accelerometer values
 
-  // read accelerometer measurements from device, scaled to the configured range
+  // read accelerometer measurements and scaled based on the above range (ax, ay, az)
   CurieIMU.readAccelerometerScaled(ax, ay, az);
 
-  // display tab-separated accelerometer x/y/z values
+  // print the x, y and z values (\t = seperated by tab)
   Serial.print("a:\t");
+  Serial.print("x-");
   Serial.print(ax);
   Serial.print("\t");
+  Serial.print("y-");
   Serial.print(ay);
   Serial.print("\t");
+  Serial.print("z-");
   Serial.print(az);
   Serial.println();
 }
-
